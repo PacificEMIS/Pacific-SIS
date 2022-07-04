bb622813024aa378726d0da44dea9885d0a70b70
Souvik Sarkar <souvik@sysalgo.com>
2022-07-04 12:47:13 +0530

    School logo upload text changed

--- UI/src/app/pages/shared-module/profile-image/profile-image.component.ts
+++ UI/src/app/pages/shared-module/profile-image/profile-image.component.ts
@@ -50,6 +50,7 @@ import { CommonService } from 'src/app/services/common.service';
 import { Router } from '@angular/router';
 import { PageRolesPermission } from 'src/app/common/page-roles-permissions.service';
 import { DefaultValuesService } from 'src/app/common/default-values.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'vex-profile-image',
@@ -90,7 +91,8 @@ export class ProfileImageComponent implements OnInit, OnDestroy {
   schoolAddModel: SchoolAddViewModel = new SchoolAddViewModel();
   AddParentInfoModel: AddParentInfoModel = new AddParentInfoModel();
   endEdit: boolean;
-  constructor(private dialog: MatDialog,
+  constructor(public translateService: TranslateService,
+    private dialog: MatDialog,
     private imageCropperService: ImageCropperService,
     private snackbar: MatSnackBar,
     private schoolService: SchoolService,
