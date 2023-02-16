82bdf80cb8388caec507b3c86caf80f029dd1477
sabyasachi_os4ed <sabyasachi.mlindia@gmail.com>
2023-02-16 14:53:25 +0530

    commit

---
+++ API/opensis.data/Migrations/MySqlMigrations/20230216092200_AlterStaffSchoolInfoMembershipId.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace opensis.data.Migrations.MySqlMigrations
+{
+    public partial class AlterStaffSchoolInfoMembershipId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "staff_school_info$FK_membership",
+                table: "staff_school_info");
+
+            migrationBuilder.DropIndex(
+                name: "IX_staff_school_info_tenant_id_school_id_membership_id",
+                table: "staff_school_info");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_staff_school_info_tenant_id_school_attached_id_membership_id",
+                table: "staff_school_info",
+                columns: new[] { "tenant_id", "school_attached_id", "membership_id" });
+
+            migrationBuilder.AddForeignKey(
+                name: "staff_school_info$FK_membership",
+                table: "staff_school_info",
+                columns: new[] { "tenant_id", "school_attached_id", "membership_id" },
+                principalTable: "membership",
+                principalColumns: new[] { "tenant_id", "school_id", "membership_id" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "staff_school_info$FK_membership",
+                table: "staff_school_info");
+
+            migrationBuilder.DropIndex(
+                name: "IX_staff_school_info_tenant_id_school_attached_id_membership_id",
+                table: "staff_school_info");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_staff_school_info_tenant_id_school_id_membership_id",
+                table: "staff_school_info",
+                columns: new[] { "tenant_id", "school_id", "membership_id" });
+
+            migrationBuilder.AddForeignKey(
+                name: "staff_school_info$FK_membership",
+                table: "staff_school_info",
+                columns: new[] { "tenant_id", "school_id", "membership_id" },
+                principalTable: "membership",
+                principalColumns: new[] { "tenant_id", "school_id", "membership_id" });
+        }
+    }
+}
