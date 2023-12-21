3ee55bf2666d4bba684d215158724090040019ff
Gopal <gdey@sysalgo.com>
2023-12-21 17:32:36 +0530

    Stududentdashboard API Temp commit

---
+++ API/opensis.core/StudentPortal/Services/StudentPortalService.cs
@@ -0,0 +1,78 @@
+﻿/***********************************************************************************
+openSIS is a free student information system for public and non-public
+schools from Open Solutions for Education, Inc.Website: www.os4ed.com.
+
+Visit the openSIS product website at https://opensis.com to learn more.
+If you have question regarding this software or the license, please contact
+via the website.
+
+The software is released under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, version 3 of the License.
+See https://www.gnu.org/licenses/agpl-3.0.en.html.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Copyright (c) Open Solutions for Education, Inc.
+
+All rights reserved.
+***********************************************************************************/
+using opensis.core.helper.Interfaces;
+using opensis.core.StudentPortal.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using opensis.data.Interface;
+using opensis.data.ViewModels.StudentPortal;
+
+namespace opensis.core.StudentPortal.Services
+{
+    public class StudentPortalService: IStudentPortalService
+    {
+        //private static string SUCCESS = "success";
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly string TOKENINVALID = "Token not Valid";
+
+        public IStudentPortalRepository studentPortalRepository;
+        public ICheckLoginSession tokenManager;
+        public StudentPortalService(IStudentPortalRepository studentPortalRepository, ICheckLoginSession checkLoginSession)
+        {
+            this.studentPortalRepository = studentPortalRepository;
+            this.tokenManager = checkLoginSession;
+        }
+        /// <summary>
+        /// Get Student Dashboard
+        /// </summary>
+        /// <param name="scheduledCourseSectionViewModelForStudent"></param>
+        /// <returns></returns>
+        public ScheduledCourseSectionViewModelForStudent GetStudentDashboard(ScheduledCourseSectionViewModelForStudent scheduledCourseSectionViewModelForStudent)
+        {
+            ScheduledCourseSectionViewModelForStudent scheduledCourseSectionViewForStudent = new ScheduledCourseSectionViewModelForStudent();
+            try
+            {
+                if(tokenManager.CheckToken(scheduledCourseSectionViewModelForStudent._tenantName+scheduledCourseSectionViewModelForStudent._userName,scheduledCourseSectionViewModelForStudent._token))
+                {
+                    scheduledCourseSectionViewForStudent = this.studentPortalRepository.GetStudentDashboard(scheduledCourseSectionViewModelForStudent);
+                }
+                else
+                {
+                    scheduledCourseSectionViewForStudent._failure=true;
+                    scheduledCourseSectionViewForStudent._message = TOKENINVALID;
+                }
+            }
+            catch (Exception ex) 
+            {
+                scheduledCourseSectionViewForStudent._failure = true;
+                scheduledCourseSectionViewForStudent._message = ex.Message;
+            }
+            return scheduledCourseSectionViewForStudent;
+        }
+    }
+}
---
+++ API/opensis.data/ViewModels/StudentPortal/CourseSectionViewListForStudent.cs
@@ -0,0 +1,90 @@
+﻿/***********************************************************************************
+openSIS is a free student information system for public and non-public
+schools from Open Solutions for Education, Inc.Website: www.os4ed.com.
+
+Visit the openSIS product website at https://opensis.com to learn more.
+If you have question regarding this software or the license, please contact
+via the website.
+
+The software is released under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, version 3 of the License.
+See https://www.gnu.org/licenses/agpl-3.0.en.html.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Copyright (c) Open Solutions for Education, Inc.
+
+All rights reserved.
+***********************************************************************************/
+using opensis.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opensis.data.ViewModels.StudentPortal
+{
+    public class CourseSectionViewListForStudent
+    {
+        public CourseSectionViewListForStudent() 
+        {
+            courseVariableSchedule = new List<CourseVariableSchedule>();
+            courseCalendarSchedule = new List<CourseCalendarSchedule>();
+            courseBlockSchedule = new List<CourseBlockSchedule>();
+            HolidayList = new List<DateTime>();
+            bellScheduleList = new List<BellSchedule>();
+        }
+        public int? CourseSectionId { get; set; }
+        public int? CourseId { get; set; }
+        public int? CalendarId { get; set; }
+        public string? CourseTitle { get; set; }
+        public int? GradeScaleId { get; set; }
+        public int? StandardGradeScaleId { get; set; }
+        public string? CourseSectionName { get; set; }
+        public DateTime? DurationStartDate { get; set; }
+        public DateTime? DurationEndDate { get; set; }
+        public int? YrMarkingPeriodId { get; set; }
+        public int? QtrMarkingPeriodId { get; set; }
+        public int? SmstrMarkingPeriodId { get; set; }
+        public int? PrgrsprdMarkingPeriodId { get; set; }
+        public string? ScheduleType { get; set; }
+        public string? CourseGradeLevel { get; set; }
+        public bool? AttendanceTaken { get; set; }
+        public string? MeetingDays { get; set; }
+        public string? ScheduledStaff { get; set; }
+        public string? WeekDays { get; set; }
+        public bool? TakeAttendanceForFixedSchedule { get; set; }
+        public bool? ConflictCourseSection { get; set; }
+        public int? AttendanceCategoryId { get; set; }
+        public string? GradeScaleType { get; set; }
+        public bool? UsedStandard { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public string? StaffFirstGivenName { get; set; }
+        public string? StaffMiddleName { get; set; }
+        public string? StaffLastFamilyName { get; set; }
+        public string? PeriodTitle { get; set; }
+        public int? BlockId { get; set; }
+        public int? PeriodId { get; set; }
+        public bool? DurationBasedOnPeriod { get; set; }
+        public bool? IsPrimaryStaff { get; set; }
+
+        public SchoolYears? SchoolYears { get; set; }
+        public Semesters? Semesters { get; set; }
+        public Quarters? Quarters { get; set; }
+        public ProgressPeriods? ProgressPeriods { get; set; }
+
+        public CourseFixedSchedule? courseFixedSchedule { get; set; }
+        public List<CourseVariableSchedule> courseVariableSchedule { get; set; }
+        public List<CourseCalendarSchedule> courseCalendarSchedule { get; set; }
+        public List<CourseBlockSchedule> courseBlockSchedule { get; set; }
+        public List<DateTime> HolidayList { get; set; }
+        public List<BellSchedule> bellScheduleList { get; set; }
+    }
+}
