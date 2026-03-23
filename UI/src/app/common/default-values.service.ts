14c130f56b6cfd92ce5358e69f18938c080ea3b0
Ghislain Hachey <1091769+ghachey@users.noreply.github.com>
2026-03-23 20:25:42 +0800

    feat(ui): support dev.local hostname for local tenant resolution

--- UI/src/app/common/default-values.service.ts
+++ UI/src/app/common/default-values.service.ts
@@ -40,7 +40,7 @@ export class DefaultValuesService {
     const url = window.location.href;
 
     let tenant = '';
-    if (url.includes('localhost')) {
+    if (url.includes('localhost') || url.includes('dev.local')) {
       sessionStorage.setItem('tenant', JSON.stringify(environment.tenant));
       tenant = environment.tenant;
     } else {
